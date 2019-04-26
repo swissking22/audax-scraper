3b2f52db384fec2daeefbd160e6147e027cbfe56
Ojong Richard <24799279+swissking22@users.noreply.github.com>
2019-04-26 13:20:07 +0100

    uploaded requirements file audax-scraper app
    Audax-scraper app crawls weather.com and returns a dictionary of 5 weather stats for city of Yaounde, CM

---
+++ audax-scraper.py
@@ -0,0 +1,28 @@
+''' 
+    Audax Scraper app that scrapes weather.com for today's weather forecast 
+    in the city of Yaounde
+'''
+
+import requests
+from bs4 import BeautifulSoup
+
+scrape_url = "https://weather.com/weather/today/l/CMXX0008:1:CM"
+
+resp = requests.get(scrape_url)
+soup = BeautifulSoup(resp.text, 'html.parser')
+
+section_card = soup.find('div', {'class': 'today_nowcard-sidecar component panel'})
+
+weather_prop_row = [ tr for tr in section_card.find_all('tr')]
+
+weather_prop_row_header = []
+weather_prop_row_values = []
+
+for row in weather_prop_row: 
+    # stores row header names in array
+    weather_prop_row_header.append(row.find('th').text.strip())
+    weather_prop_row_values.append((row.td.span).text.strip())
+
+weather_set = dict(zip(weather_prop_row_header, weather_prop_row_values))
+
+print(weather_set)
